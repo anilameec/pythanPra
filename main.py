646a3774d9cc5ae1399f567de851f855f32391d4
anilameec <anilameec@gmail.com>
2021-11-28 09:54:44 -0600

    Update main.py
    zz

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm nadatta')
+    print_hi('PyCharm nadatta abcde')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
